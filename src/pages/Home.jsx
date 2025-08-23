b8f40443fa20e8790990a1e069a5698ae940c290
isha-03 <ijain2_be21@thapar.edu>
2025-08-23 15:23:46 +0530

    easy-health frontend

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+
+import SpecialityMenu from '../components/SpecialityMenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      
+    </div>
+  )
+}
+
+export default Home
